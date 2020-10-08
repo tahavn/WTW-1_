774c89396c18a3ddf16474c49f5ba135683712c7
Mikchail <refs1@yandex.ru>
2020-10-08 18:34:47 +0400

    add video func

--- src/components/addreview/addreview.jsx
+++ src/components/addreview/addreview.jsx
@@ -1,111 +1,111 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import Header from '../header/header';
 import Breadcrumbs from '../breadcrumbs/breadcrumbs';
-const AddReview = (props) => {
-  const {id} = props.match.params;
-  return (
-    <section className="movie-card movie-card--full">
-      <div className="movie-card__header">
-        <div className="movie-card__bg">
-          <img
-            src="img/bg-the-grand-budapest-hotel.jpg"
-            alt="The Grand Budapest Hotel"
-          />
-        </div>
+import {render} from 'react-dom';
 
-        <h1 className="visually-hidden">WTW</h1>
+class AddReview extends PureComponent {
+  constructor(props) {
+    super();
+    this.state = {
+      rating: 0,
+      'review-text': ``,
+    };
+    this.rating = [1, 2, 3, 4, 5];
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+  handleChange(event) {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value,
+    });
+    console.log(this.state);
+  }
+  render() {
+    const {id} = this.props.match.params;
+    console.log(`рендер`);
 
-        <Header Breadcrumbs={Breadcrumbs} id={id} />
-
-        <div className="movie-card__poster movie-card__poster--small">
-          <img
-            src="/img/the-grand-budapest-hotel-poster.jpg"
-            alt="The Grand Budapest Hotel poster"
-            width="218"
-            height="327"
-          />
-        </div>
-      </div>
-
-      <div className="add-review">
-        <form action="#" className="add-review__form">
-          <div className="rating">
-            <div className="rating__stars">
-              <input
-                className="rating__input"
-                id="star-1"
-                type="radio"
-                name="rating"
-                value="1"
-              />
-              <label className="rating__label" htmlFor="star-1">
-                Rating 1
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-2"
-                type="radio"
-                name="rating"
-                value="2"
-              />
-              <label className="rating__label" htmlFor="star-2">
-                Rating 2
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-3"
-                type="radio"
-                name="rating"
-                value="3"
-                checked
-              />
-              <label className="rating__label" htmlFor="star-3">
-                Rating 3
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-4"
-                type="radio"
-                name="rating"
-                value="4"
-              />
-              <label className="rating__label" htmlFor="star-4">
-                Rating 4
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-5"
-                type="radio"
-                name="rating"
-                value="5"
-              />
-              <label className="rating__label" htmlFor="star-5">
-                Rating 5
-              </label>
-            </div>
+    return (
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__header">
+          <div className="movie-card__bg">
+            <img
+              src="img/bg-the-grand-budapest-hotel.jpg"
+              alt="The Grand Budapest Hotel"
+            />
           </div>
 
-          <div className="add-review__text">
-            <textarea
-              className="add-review__textarea"
-              name="review-text"
-              id="review-text"
-              placeholder="Review text"
-            ></textarea>
-            <div className="add-review__submit">
-              <button className="add-review__btn" type="submit">
-                Post
-              </button>
-            </div>
+          <h1 className="visually-hidden">WTW</h1>
+
+          <Header Breadcrumbs={Breadcrumbs} id={id} />
+
+          <div className="movie-card__poster movie-card__poster--small">
+            <img
+              src="/img/the-grand-budapest-hotel-poster.jpg"
+              alt="The Grand Budapest Hotel poster"
+              width="218"
+              height="327"
+            />
           </div>
-        </form>
-      </div>
-    </section>
-  );
-};
+        </div>
+
+        <div className="add-review">
+          <form
+            action="#"
+            className="add-review__form"
+            onSubmit={(event) => this.handleSubmit(event)}
+          >
+            <div className="rating">
+              <div className="rating__stars">
+                {this.rating.map((item, index) => {
+                  const checked = +this.state.rating === item ? true : false;
+                  return (
+                    <React.Fragment key={item + index}>
+                      <input
+                        onChange={(event) => {
+                          event.preventDefault();
+                          this.handleChange(event);
+                        }}
+                        className="rating__input"
+                        id={`star-${item}`}
+                        type="radio"
+                        name="rating"
+                        value={`${item}`}
+                        checked={checked}
+                      />
+                      <label className="rating__label" htmlFor={`star-${item}`}>
+                        Rating {item}
+                      </label>
+                    </React.Fragment>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="add-review__text">
+              <textarea
+                onChange={(event) => {
+                  this.handleChange(event);
+                }}
+                className="add-review__textarea"
+                name="review-text"
+                id="review-text"
+                placeholder="Review text"
+              ></textarea>
+              <div className="add-review__submit">
+                <button className="add-review__btn" type="submit">
+                  Post
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default AddReview;
--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,57 +1,155 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
+import {render} from 'react-dom';
+const TimeRanges = {
+  MINUTES_IN_HOUR: 60,
+  SECONDS_IN_MINUTE: 60,
+};
+class Player extends PureComponent {
+  constructor(props) {
+    super(props);
 
-const Player = (props) => {
-  const id = props.match.params.id;
-  const {films} = props;
-  const film = films[id];
-  console.log(film);
-  return (
-    <div className="player">
-      <video
-        autoPlay={true}
-        src="https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm"
-        className="player__video"
-        poster="/img/player-poster.jpg"
-      ></video>
+    this.state = {
+      currentTime: 0,
+      duration: 0,
+      isPlaying: true,
+    };
 
-      <button type="button" className="player__exit">
-        Exit
-      </button>
+    this.videoRef = React.createRef();
 
-      <div className="player__controls">
-        <div className="player__controls-row">
-          <div className="player__time">
-            <progress
-              className="player__progress"
-              value="30"
-              max="100"
-            ></progress>
-            <div className="player__toggler" style={{left: '30%'}}>
-              Toggler
+    this._handleIsPlayingChange = this._handleIsPlayingChange.bind(this);
+    this._handleSetFullScreen = this._handleSetFullScreen.bind(this);
+  }
+
+  componentDidMount() {
+    const {film} = this.props;
+    const video = this.videoRef.current;
+    video.src = film.srcMovie;
+    video.play();
+    video.onloadedmetadata = () =>
+      this.setState({
+        duration: video.duration,
+      });
+    video.ontimeupdate = () =>
+      this.setState({
+        currentTime: Math.trunc(video.current),
+      });
+  }
+
+  componentDidUpdate() {
+    const video = this.videoRef.current;
+    if (document.fullscreenElement === null) {
+      video.controls = false;
+    }
+    if (this.state.isPlaying) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
+  componentWillMount() {
+    const video = this.videoRef.current;
+
+    video.src = ``;
+    video.onplay = null;
+    video.onloadedmetadata = null;
+    video.ontimeupdate = null;
+    video.controls = null;
+  }
+
+  _handleIsPlayingChange() {
+    const {isPlaying} = this.state;
+    this.setState({
+      isPlaying: !isPlaying,
+    });
+  }
+  _handleSetFullScreen() {
+    const video = this.videoRef.current;
+    video.requestFullscreen();
+    video.controls = true;
+  }
+  _leftTime() {
+    const {currentTime, duration} = this.state;
+    const timeDiff = duration - currentTime;
+    const second = Math.trunc(timeDiff % TimeRanges.SECONDS_IN_MINUTE);
+    const minutes = Math.trunc(timeDiff % TimeRanges.SECONDS_IN_MINUTE);
+    const hours = Math.trunc(timeDiff % TimeRanges.MINUTES_IN_HOUR);
+
+    return `${hours.toString().padStart(2, `0`)}:${minutes
+      .toString()
+      .padStart(2, `0`)}${second.toString().padStart(2, `0`)}`;
+  }
+  // const playerToggler = (currentTime * 100) / duration + `%`;
+
+  render() {
+    const id = this.props.match.params.id;
+    const {films} = this.props;
+    const film = films[id];
+    const {isPlaying, currentTime, duration} = this.state;
+    const btnIsPlaying = isPlaying ? (
+      <React.Fragment>
+        <svg viewBox="0 0 14 21" width="14" height="21">
+          <use xlinkHref="#pause"></use>
+        </svg>
+        <span>Pause</span>
+      </React.Fragment>
+    ) : (
+      <React.Fragment>
+        <svg viewBox="0 0 19 19" width="19" height="19">
+          <use xlinkHref="#play-s"></use>
+        </svg>
+        <span>Play</span>
+      </React.Fragment>
+    );
+    return (
+      <div className="player">
+        <video
+          ref={this.videoRef}
+          
+          className="player__video"
+          poster="/img/player-poster.jpg"
+        ></video>
+
+        <button type="button" className="player__exit">
+          Exit
+        </button>
+
+        <div className="player__controls">
+          <div className="player__controls-row">
+            <div className="player__time">
+              <progress
+                className="player__progress"
+                value="30"
+                max="100"
+              ></progress>
+              <div className="player__toggler" style={{left: '30%'}}>
+                Toggler
+              </div>
             </div>
+            <div className="player__time-value">1:30:29</div>
           </div>
-          <div className="player__time-value">1:30:29</div>
-        </div>
 
-        <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use href="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
-          <div className="player__name">Transpotting</div>
+          <div className="player__controls-row">
+            <button
+              type="button"
+              className="player__play"
+              onClick={() => this._handlePlayingChange()}
+            >
+              {btnIsPlaying}
+            </button>
+            <div className="player__name">Transpotting</div>
 
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use href="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
+            <button type="button" className="player__full-screen">
+              <svg viewBox="0 0 27 27" width="27" height="27">
+                <use href="#full-screen"></use>
+              </svg>
+              <span>Full screen</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Player;
