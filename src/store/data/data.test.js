35401661f5263cbc9beddecc86699af0e645adde
Mike <Mike>
2020-11-13 14:37:24 +0400

    add async test

--- src/store/data/data.test.js
+++ src/store/data/data.test.js
@@ -1,9 +1,28 @@
 /* eslint-disable indent */
 import MockAdapter from 'axios-mock-adapter';
-import {reducer, ActionCreator, ActionType} from './data-reducer';
+import {adaptiveFilms} from '../../adapter';
+import {reducer, ActionCreator, ActionType, Operations} from './data-reducer';
 import {createAPI} from '../../api';
 import {films} from '../../../mocks/films';
-
+const mockFilmServerStyle = {
+  background_color: ``,
+  background_image: ``,
+  description: `I hate tests.`,
+  director: ``,
+  genre: ``,
+  id: 1,
+  is_favorite: true,
+  name: ``,
+  poster_image: ``,
+  preview_image: ``,
+  preview_video_link: ``,
+  rating: 3.6,
+  released: 2008,
+  run_time: 92,
+  scores_count: 0,
+  starring: [``, ``, ``],
+  video_link: ``,
+};
 const initialState = {
   films: [],
   isLoading: false,
@@ -29,20 +48,41 @@ describe(`should test data-reducer`, () => {
   });
 
   it(`should update  load films`, () => {
-      expect(
-        reducer(
-          {
-            films: [],
-            isLoading: false,
-          },
-          {
-            type: ActionType.LOAD_FILMS,
-            payload: films,
-          }
-        )
-      ).toEqual({
-        films,
-        isLoading: true,
+    expect(
+      reducer(
+        {
+          films: [],
+          isLoading: false,
+        },
+        {
+          type: ActionType.LOAD_FILMS,
+          payload: films,
+        }
+      )
+    ).toEqual({
+      films,
+      isLoading: true,
+    });
+  });
+});
+
+describe('Async operation work correctly', () => {
+  it('Shuld make a correct Api call to /films', () => {
+    const dataMock = [mockFilmServerStyle];
+    const dispatch = jest.fn();
+    const filmLoader = Operations.loadFilms();
+
+    apiMock.onGet(`films`).reply(200, dataMock);
+
+    return filmLoader(dispatch, () => {}, api).then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: ActionType.LOAD_FILMS,
+        payload: dataMock.map((film) => adaptiveFilms(film)),
       });
+      expect(dispatch).toHaveBeenNthCalledWith(2,{
+        type: ActionType.IS_LOAD_FILMS,
+      })
+    });
   });
 });
